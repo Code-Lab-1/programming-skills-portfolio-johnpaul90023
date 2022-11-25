521dac3e9c492f08dec0557e8736313d26dc6860
johnpaul90023 <johnpaultgonzales@gmail.com>
2022-11-25 22:30:47 +0400

    Chapter 7
    John Paul Gonzales

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+#Displaying a message using def function
+
+def display_message():
+    msg = " \nI am going to learn how to store codes in functions and learn different kinds of programming words in the lesson functions. \n"
+    print(msg)
+display_message()
---
+++ Chapter 7- Functions/Exercises/Exercise6.py
@@ -0,0 +1,4 @@
+#The function generates a message with a paremeter that only contains certain default countries.
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+print(len(numbers))
