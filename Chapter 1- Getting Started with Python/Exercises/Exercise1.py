f86af6fdf979a3fdf6b77453c73fc3d0e6bea5d4
johnpaul90023 <johnpaultgonzales@gmail.com>
2022-11-25 22:29:24 +0400

    Chapter 1

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+#Prints the twinkle twinkle song lyrics in each line
+
+print("Twinkle, twinkle, little star")
+print("How i wonder what you are!")
+print("Up above the world so high")
+print("Like a diamond in the sky")
+print("Twinkle, twinkle, little star")
+print("How i wonder what you are")
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise7.py
@@ -0,0 +1,7 @@
+#Using parentheses to indicate on the operators in what to perform first
+q = 5 
+e = 4 
+r = 8 
+solution = q*(e+r)
+print(solution)
+
